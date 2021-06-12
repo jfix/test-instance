88cbac1887824e4da66b0e022cd08d8228da1c8e
Jakob Fix <jakob.fix@gmail.com>
2021-06-12 23:13:11 +0200

    Adapt index.php and secrets

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,12 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->load();
+    require_once __DIR__.'/../vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+    $dotenv->load();
+    $dotenv->required(['A', 'B', 'C']);
 ?>
-
-Hello PHP!
-
-<?=$_ENV['DB_USER'] ?>
+<html>
+<h1>Hello PHP!</h1>
+<br/> A <?php var_dump( $_ENV['A'] ); ?>
+<br/> B <?php var_dump( $_ENV['B'] ); ?>
+<br/> C <?php var_dump( $_ENV['C'] ); ?>
+</html>
